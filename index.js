10d099c2c172aec034eae21f113f045945cfe947
Camila Martinez <camila.martinez@itam.mx>
2021-11-18 15:11:42 -0600

    Archivos restantes

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ const app= express();
 
 app.use(express.static(_dirname+'/public'));
 
+app.get('/hola',(req,res)=>{
+    res.send('ruta Hola');
 
+});
+app.get('/adios',(req,res)=>{
+    res.send('ruta Adios');
+
+});
+
+app.post('/registro',(req,res)=>{
+    res.send('recibiendo informacion de registro');
+});
+
+app.post('/login',(req,res)=>{
+    res.send('ruta de login nueva');
+});
 
 app.listen(3000,() =>{
     console.log('Example app listening on port 3000!')
